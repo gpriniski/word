a686927612a54d048c637e2dd7a2d19aed1b02c4
George Priniski <priniskigeorge@gmail.com>
2016-05-07 01:33:34 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Word::Application.config.secret_key_base = '21529415824df76decd63a00c5480a9c2549e839b82f114513f2605c8ef07ab6cf37ebfcf9acfab920687b3834fde9361bd60dc213b2c9f0d0215eddfbf2bbe8'
